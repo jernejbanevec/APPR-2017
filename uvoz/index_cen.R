f1a15d094a0b2f035152fb64c09d1c1da3586bf5
Jernej Banevec <jernej.banevec@gmail.com>
2017-05-08 12:01:02 +0200

    sprememba pri uvozu xml in in uporabil funkcijo select pri uvozu podatkov ter spremenil sumnike

--- uvoz/index_cen.R
+++ uvoz/index_cen.R
@@ -2,28 +2,8 @@ library(readr)
 library(reshape2)
 library(dplyr)
 
-consumer.price.index <- read_csv("podatki/consumer_price_index.csv", col_names = c("i", "p", "i2", "DRŽAVA", "i3", "i4", "i5", "i6", "LETO", "i7", "i8", "ŠTA", "i9", "i10", "i11", "i12", "INDEX CEN", "i13", "i14"), skip = 1, na = '-', locale = locale(encoding = "Windows-1250"))
-
-consumer.price.index$i1 <- NULL
-consumer.price.index$i2 <- NULL
-consumer.price.index$i3 <- NULL
-consumer.price.index$i4 <- NULL
-consumer.price.index$i5 <- NULL
-consumer.price.index$i6 <- NULL
-consumer.price.index$i7 <- NULL
-consumer.price.index$i8 <- NULL
-consumer.price.index$i9 <- NULL
-consumer.price.index$i10 <- NULL
-consumer.price.index$i11 <- NULL
-consumer.price.index$i12 <- NULL
-consumer.price.index$i13 <- NULL
-consumer.price.index$i14 <- NULL
-consumer.price.index$i <- NULL
-consumer.price.index <- consumer.price.index %>% filter(consumer.price.index$p == "Consumer prices - all items")
-consumer.price.index <- consumer.price.index %>% filter(consumer.price.index$ŠTA == "Index")
-consumer.price.index$p <- NULL
-consumer.price.index$ŠTA <- NULL
-consumer.price.index$`INDEX CEN` <- as.numeric(consumer.price.index$`INDEX CEN`)
-consumer.price.index$`LETO` <- as.numeric(consumer.price.index$`LETO`)
-View(consumer.price.index)
+consumer.price.index <- read_csv("podatki/consumer_price_index.csv", col_names = c("i", "p", "i2", "DRZAVA", "i3", "i4", "i5", "i6", "LETO", "i7", "i8", "STA", "i9", "i10", "i11", "i12", "INDEX.CEN", "i13", "i14"), 
+                                 skip = 1, na = '-', 
+                                 locale = locale(encoding = "UTF-8")) %>%
+  filter(p == "Consumer prices - all items", STA == "Index") %>% select(DRZAVA, LETO, INDEX.CEN)
 
--- uvoz/uvoz_st_splavov_sola_zadnja.R
+++ uvoz/uvoz_st_splavov_sola_zadnja.R
@@ -1,16 +1,13 @@
 library(rvest)
+library(readr)
 xml <- file("podatki/stevilo_splavov.xml") %>% read_xml()
 records <- xml %>% xml_nodes(xpath="//record")
 fields <- records[[1]] %>% xml_nodes(xpath="./field") %>% xml_attr("name")
-data <- sapply(fields[-length(fields)], function(x) {
-  +     sapply(records, . %>% xml_nodes(xpath=sprintf("./field[@name='%s']", x)) %>%
-                 +                xml_contents() %>% as.character())
-  + }) %>% data.frame()
-names(data) <- c("DRŽAVA", "LETO", "i1", "i2", "i3", "i4", "ŠTEVILO SPLAVOV")
+data <- sapply(fields[-length(fields)], function(x) {     sapply(records, . %>% xml_nodes(xpath=sprintf("./field[@name='%s']", x)) %>%      xml_contents() %>% as.character()) }) %>% data.frame()
+names(data) <- c("DRZAVA", "LETO", "i1", "i2", "i3", "i4", "STEVILO.SPLAVOV")
 data$i1 <- NULL
 data$i2 <- NULL
 data$i3 <- NULL
 data$i4 <- NULL
-data$LETO <- as.numeric(data$LETO)
-data$`ŠTEVILO SPLAVOV`<- as.numeric(data$`ŠTEVILO SPLAVOV`)
-View(data)
+data$LETO <- parse_number(data$LETO)
+data$`STEVILO SPLAVOV`<- parse_number(data$`STEVILO.SPLAVOV`)
