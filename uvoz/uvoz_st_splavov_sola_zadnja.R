7452f1718ca9d1531a068e512366c152748306b8
Uporabnik <Uporabnik>
2017-05-18 15:15:23 +0200

    preverjal če deluje uredu

--- uvoz/uvoz_st_splavov_sola_zadnja.R
+++ uvoz/uvoz_st_splavov_sola_zadnja.R
@@ -4,8 +4,8 @@ xml <- file("podatki/stevilo_splavov.xml") %>% read_xml()
 records <- xml %>% xml_nodes(xpath="//record")
 fields <- records[[1]] %>% xml_nodes(xpath="./field") %>% xml_attr("name")
 data <- sapply(fields[-length(fields)], function(x) {     sapply(records, . %>% xml_nodes(xpath=sprintf("./field[@name='%s']", x)) %>%      xml_contents() %>% as.character()) }) %>% data.frame()
-names(data) <- c("DRZAVA", "LETO", "i1", "i2", "i3", "i4", "STEVILO.SPLAVOV")
-data$i1 <- NULL
+names(data) <- c("DRZAVA", "LETO", "i8", "i2", "i3", "i4", "STEVILO.SPLAVOV")
+data$i8 <- NULL
 data$i2 <- NULL
 data$i3 <- NULL
 data$i4 <- NULL
