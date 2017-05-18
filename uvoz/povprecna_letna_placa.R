d815ed42bc2f962ae600aa762a2f92ac8c5d7e0c
Uporabnik <Uporabnik>
2017-05-18 21:55:36 +0200

    uvoz

--- uvoz/povprecna_letna_placa.R
+++ uvoz/povprecna_letna_placa.R
@@ -5,11 +5,7 @@ library(dplyr)
 average.annual.wage <- read_csv("podatki/average_annual_wage.csv", 
                                 col_names = c("DRZAVA", "i1", "i2", "i3", "i4", "LETO", "POVPRECNA.LETNA.PLACA", "i5"), 
                                 skip = 1, na = '-', 
-                                locale = locale(encoding = "Windows-1250"))
-average.annual.wage$i1 <- NULL
-average.annual.wage$i2 <- NULL
-average.annual.wage$i3 <- NULL
-average.annual.wage$i4 <- NULL
-average.annual.wage$i5 <- NULL
-average.annual.wage$`LETO` <- as.numeric(average.annual.wage$`LETO`)
-average.annual.wage$`POVPRECNA.LETNA.PLACA`<- as.numeric(average.annual.wage$`POVPRECNA.LETNA.PLACA`)
+                                locale = locale(encoding = "Windows-1250")) %>%
+  select(DRZAVA, LETO, POVPRECNA.LETNA.PLACA)
+average.annual.wage$`LETO` <- parse_number(average.annual.wage$`LETO`)
+average.annual.wage$`POVPRECNA.LETNA.PLACA`<- parse_number(average.annual.wage$`POVPRECNA.LETNA.PLACA`)
