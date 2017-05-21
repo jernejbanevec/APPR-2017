6d2117562c1c920f5081405598537ba4f171cb08
Uporabnik <Uporabnik>
2017-05-21 23:41:16 +0200

    poskusal spremeniti ime drzave, commitam da lahko jutri pokazem na vajah

--- uvoz/nataliteta.R
+++ uvoz/nataliteta.R
@@ -4,6 +4,11 @@ library(dplyr)
 crude.birth.rate <- read_csv("podatki/crude_birth_rateCSV.csv", 
                              col_names = c("LETO", "DRZAVA", "i", "NATALITETA", "i2"), 
                              skip = 1, na =c("", " ", "-", ":")) %>%
-  select(DRZAVA, LETO, NATALITETA)
+  select(DRZAVA, LETO, NATALITETA)   
+
 crude.birth.rate$NATALITETA <- parse_number(crude.birth.rate$NATALITETA)
 crude.birth.rate$LETO <- parse_number(crude.birth.rate$LETO)
+crude.birth.rate$DRZAVA <- gsub("Germany (including former GDR)", "Germany", crude.birth.rate$DRZAVA)
+
+if crude.birth.rate$DRZAVA == "Germany (including former GDR)":
+  crude.birth.rate$DRZAVA <- "Germany"
