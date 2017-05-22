2e73097a309de6be602f84a928707c94db2045a0
Jernej Banevec <jernej.banevec@gmail.com>
2017-05-22 19:10:03 +0200

    začetek vizualizacije

--- vizualizacija/normirano_st_splavov.R
+++ vizualizacija/normirano_st_splavov.R
@@ -1,6 +1,14 @@
+#NOMINALNO STEVILO SPLAVOV NA 1000 PREBIVALCEV
+
 
 zdruzeno <- left_join(data, average.population)
 
 #pobrišemo vrstice imajo vrednost "NA"
 row.has.na <- apply(zdruzeno, 1, function(x){any(is.na(x))})
 zdruzeno <- zdruzeno[!row.has.na,]
+
+#stevilo splavov na 1000 prebivalcev drzave
+nominalno.stevilo.splavov <- zdruzeno %>% 
+  group_by(DRZAVA, LETO) %>% 
+  summarise(NOM.STEVILO.SPLAVOV = STEVILO.SPLAVOV / POPULACIJA * 1000)
+
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,13 +1,46 @@
 # 3. faza: Vizualizacija podatkov
+library(ggplot2)
+library(dplyr)
+library(readr)
+library(tibble)
 
-# Uvozimo zemljevid.
-zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip",
-                             "OB/OB", encoding = "Windows-1250")
-levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
-  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
-zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels = levels(obcine$obcina))
-zemljevid <- pretvori.zemljevid(zemljevid)
+#Uvozimo ZEMLJEVID
 
-# Izračunamo povprečno velikost družine
-povprecja <- druzine %>% group_by(obcina) %>%
-  summarise(povprecje = sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
+evropa <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip",
+                          "ne_50m_admin_0_countries", encoding = "UTF-8") %>%
+  pretvori.zemljevid() %>% filter(continent == "Europe" | sovereignt %in% c("Turkey", "Cyprus"),
+                                  long > -30, sovereignt != "Russia")
+
+#prikaz zemljevida brez podatkov in oznak
+ggplot() +  geom_polygon(data = evropa, aes(x = long, y = lat, group = group)) +
+  coord_map(xlim = c(-25, 40), ylim = c(32, 72))
+
+# REALNA PLACA ZA DOLOČENO DRZAVO NA DOLOCENO LETO GLEDE NA DEN INDEX CEN (zaenkrat zgolj podatki)
+
+sestavljen.data.frame <- left_join(average.annual.wage, consumer.price.index);
+
+#pobrišemo vrstice imajo vrednost "NA"
+row.has.na <- apply(sestavljen.data.frame, 1, function(x){any(is.na(x))})
+sestavljen.data.frame <- sestavljen.data.frame[!row.has.na,]
+
+#stevilo splavov na 1000 prebivalcev drzave
+real.wage <- sestavljen.data.frame %>% 
+  group_by(DRZAVA, LETO) %>% 
+  summarise(REALNA.PLACA = POVPRECNA.LETNA.PLACA / INDEX.CEN * 100)
+
+
+
+
+
+#NOMINALNO STEVILO SPLAVOV NA 1000 PREBIVALCEV (zaenkrat zgolj podatki)
+
+zdruzeno <- left_join(data, average.population)
+
+#pobrišemo vrstice imajo vrednost "NA"
+row.has.na <- apply(zdruzeno, 1, function(x){any(is.na(x))})
+zdruzeno <- zdruzeno[!row.has.na,]
+
+#stevilo splavov na 1000 prebivalcev drzave
+nominalno.stevilo.splavov <- zdruzeno %>% 
+  group_by(DRZAVA, LETO) %>% 
+  summarise(NOM.STEVILO.SPLAVOV = STEVILO.SPLAVOV / POPULACIJA * 1000)
