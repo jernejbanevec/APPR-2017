4117699aeb6e97b184d65e399eb46bd2950572e5
Jernej Banevec <jernej.banevec@gmail.com>
2017-06-06 16:57:38 +0200

    Posodobil Project.Rmd ter prilagodil uvoz in vizualizacijo.

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -117,3 +117,30 @@ average.population$DRZAVA <- gsub("^Germany.*$", "Germany", average.population$D
 average.population$POPULACIJA <-gsub(",","",average.population$POPULACIJA)
 average.population$POPULACIJA <- parse_number(average.population$POPULACIJA)
 
+# REALNA PLACA ZA DOLOČENO DRZAVO NA DOLOCENO LETO GLEDE NA DEN INDEX CEN (zaenkrat zgolj podatki)
+
+sestavljen.data.frame <- left_join(average.annual.wage, consumer.price.index);
+
+#pobrišemo vrstice imajo vrednost "NA"
+row.has.na <- apply(sestavljen.data.frame, 1, function(x){any(is.na(x))})
+sestavljen.data.frame <- sestavljen.data.frame[!row.has.na,]
+
+#stevilo splavov na 1000 prebivalcev drzave
+real.wage <- sestavljen.data.frame %>% 
+  group_by(DRZAVA, LETO) %>% 
+  summarise(REALNA.PLACA = POVPRECNA.LETNA.PLACA / INDEX.CEN * 100)
+
+
+
+#NOMINALNO STEVILO SPLAVOV NA 1000 PREBIVALCEV (zaenkrat zgolj podatki)
+
+zdruzeno <- left_join(data, average.population)
+
+#pobrišemo vrstice imajo vrednost "NA"
+row.has.na <- apply(zdruzeno, 1, function(x){any(is.na(x))})
+zdruzeno <- zdruzeno[!row.has.na,]
+
+#stevilo splavov na 1000 prebivalcev drzave
+nominalno.stevilo.splavov <- zdruzeno %>% 
+  group_by(DRZAVA, LETO) %>% 
+  summarise(NOM.STEVILO.SPLAVOV = STEVILO.SPLAVOV / POPULACIJA * 1000)
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -15,70 +15,55 @@ evropa <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturaleart
 #prikaz zemljevida brez podatkov in oznak
 
 ggplot() +  geom_polygon(data = evropa, aes(x = long, y = lat, group = group)) +
-  coord_map(xlim = c(-25, 40), ylim = c(32, 72))
+  coord_map(xlim = c(-25, 40), ylim = c(32, 72)) 
 
 
 #poskrbim da se ujemajo imena
 
 evropa$name_sort <- gsub("^Slovak Republic$", "Slovakia", evropa$name_sort) %>% factor() #zemljevid
 
-# REALNA PLACA ZA DOLOČENO DRZAVO NA DOLOCENO LETO GLEDE NA DEN INDEX CEN (zaenkrat zgolj podatki)
-
-sestavljen.data.frame <- left_join(average.annual.wage, consumer.price.index);
-
-#pobrišemo vrstice imajo vrednost "NA"
-row.has.na <- apply(sestavljen.data.frame, 1, function(x){any(is.na(x))})
-sestavljen.data.frame <- sestavljen.data.frame[!row.has.na,]
-
-#stevilo splavov na 1000 prebivalcev drzave
-real.wage <- sestavljen.data.frame %>% 
-  group_by(DRZAVA, LETO) %>% 
-  summarise(REALNA.PLACA = POVPRECNA.LETNA.PLACA / INDEX.CEN * 100)
-
-
-
-#NOMINALNO STEVILO SPLAVOV NA 1000 PREBIVALCEV (zaenkrat zgolj podatki)
-
-zdruzeno <- left_join(data, average.population)
-
-#pobrišemo vrstice imajo vrednost "NA"
-row.has.na <- apply(zdruzeno, 1, function(x){any(is.na(x))})
-zdruzeno <- zdruzeno[!row.has.na,]
-
-#stevilo splavov na 1000 prebivalcev drzave
-nominalno.stevilo.splavov <- zdruzeno %>% 
-  group_by(DRZAVA, LETO) %>% 
-  summarise(NOM.STEVILO.SPLAVOV = STEVILO.SPLAVOV / POPULACIJA * 1000)
 
 #graf, ki pokaže število splavov na 1000 prebivalcev za izbrane države
 
-splavi <- ggplot(data = nominalno.stevilo.splavov %>% filter(DRZAVA %in% c("Portugal", "Slovenia", "Poland", "Romania")), aes(x = LETO, y = NOM.STEVILO.SPLAVOV)) + geom_point() + geom_line(col="red") + facet_wrap(~DRZAVA, ncol=4)
+splavi <- ggplot(data = nominalno.stevilo.splavov %>% filter(DRZAVA %in% c("Portugal", "Slovenia", "Poland", "Romania")), aes(x = LETO, y = NOM.STEVILO.SPLAVOV)) + 
+  geom_point() + geom_line(col="red") + 
+  facet_wrap(~DRZAVA, ncol=4) + 
+  ylab("Število splavov na 1000 prebivalcev") +
+  xlab("Leto") +
+  ggtitle("Število splavov")
 
 #graf, ki pokaže spreminjanje realne plače za Grčijo in Španijo, katere imata trenutno slabo finančno stanje ter Švico 
 
-rojstva <- ggplot(data = crude.birth.rate %>% filter(DRZAVA %in% c("Greece", "Spain", "Switzerland")), aes(x = LETO, y = NATALITETA)) + geom_point() + geom_line(col="red") + facet_wrap(~DRZAVA, ncol=3)
+rojstva <- ggplot(data = crude.birth.rate %>% filter(DRZAVA %in% c("Greece", "Spain", "Switzerland")), aes(x = LETO, y = NATALITETA)) + 
+  geom_point() + 
+  geom_line(col="red") + 
+  facet_wrap(~DRZAVA, ncol=3) + 
+  ylab("Število rojstev na 1000 prebivalcev") +
+  xlab("Leto") +
+  ggtitle("Nataliteta")
 
 #oznake na zemljevidu, katerih velikost pove realno plačo in barva število splavljenih otrok
 
-ggplot() + geom_polygon(data = evropa %>%
-                          left_join(nominalno.stevilo.splavov %>% filter(LETO == 2005),
+zemljevid.splavi <- ggplot() + geom_polygon(data = evropa %>%
+                          left_join(nominalno.stevilo.splavov %>% filter(LETO == 2011),
                                     by = c("name_long" = "DRZAVA")),
                         aes(x = long, y = lat, group = group, fill = NOM.STEVILO.SPLAVOV)) +
   geom_point(data = evropa %>% filter(long >= -25, long <= 40, lat >= 32, lat <= 79) %>%
                group_by(name_long) %>% summarise(x = mean(long), y= mean(lat)) %>%
-               inner_join(real.wage %>% filter(LETO == 2005), by = c("name_long" = "DRZAVA")),
+               inner_join(real.wage %>% filter(LETO == 2011), by = c("name_long" = "DRZAVA")),
              aes(x = x, y = y, size = REALNA.PLACA))+
-  coord_map(xlim = c(-25, 40), ylim = c(32, 72))
+  coord_map(xlim = c(-25, 40), ylim = c(32, 72)) + scale_fill_gradient(low = "#3C0000", high = "#F00000") + 
+  ggtitle("Število splavov v primerjavi z realno plačo")
 
 
 #oznake na zemljevidu, katerih velikost pove realno plačo in število novorojenih otrok
 
-ggplot() + geom_polygon(data = evropa %>%
-                          left_join(crude.birth.rate %>% filter(LETO == 2005),
+zemljevid.nataliteta <- ggplot() + geom_polygon(data = evropa %>%
+                          left_join(crude.birth.rate %>% filter(LETO == 2015),
                                     by = c("name_long" = "DRZAVA")),
                         aes(x = long, y = lat, group = group, fill = NATALITETA)) +
   geom_point(data = evropa %>% filter(long >= -25, long <= 40, lat >= 32, lat <= 79) %>%
                group_by(name_long) %>% summarise(x = mean(long), y= mean(lat)) %>%
-               inner_join(real.wage %>% filter(LETO == 2005), by = c("name_long" = "DRZAVA")),
+               inner_join(real.wage %>% filter(LETO == 2015), by = c("name_long" = "DRZAVA")),
              aes(x = x, y = y, size = REALNA.PLACA))+
-  coord_map(xlim = c(-25, 40), ylim = c(32, 72))
+  coord_map(xlim = c(-25, 40), ylim = c(32, 72)) + ggtitle("Število rojstev v primerjavi z realno plačo")
