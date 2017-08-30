e7f40eaccbc5999c36db0397e9c4188969d86dde
Uporabnik <Uporabnik>
2017-08-30 12:07:28 +0200

    Dodal dodatne knjiznice

--- lib/libraries.r
+++ lib/libraries.r
@@ -6,6 +6,9 @@ library(gsubfn)
 library(ggplot2)
 library(reshape2)
 library(shiny)
+library(tibble)
+library(mapproj)
+library(GGally)
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding = "UTF-8")
